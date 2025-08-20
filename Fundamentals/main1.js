298a9cfbcc24706fb2fcc710077c41e16ec044de
ASH41B <aishwaryaburte41@gmail.com>
2025-08-20 11:25:54 +0530

    auto-commit

---
+++ Fundamentals/main1.js
@@ -0,0 +1,6 @@
+//console.log("Connected from main1.js");
+
+//alert("hi from an Alert");
+//document.write ('This is from the doc.write function');
+
+document.getElementById("test").innerHTML="Hello from inner html";
